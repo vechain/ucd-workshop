9bfcb045ebbae0c715fd0d74441b8b4edcdfcc6b
Kostas Apostolopoulos <kostas.apostolopoulos@vechain.org>
2025-11-19 19:36:52 +0000

    Add comment to highlight logging

--- agent/src/vechain/account.js
+++ agent/src/vechain/account.js
@@ -5,6 +5,9 @@ export async function getAccountBalances(address, network = process.env.VECHAIN_
 
   const account = await thor.accounts.getAccount(address);
 
+  // Feel free to add logs, for example:
+  console.log("You can add console logs here to debug your code, they'll show up in the terminal!");
+
   // Convert raw balances (in wei) to readable VET and VTHO
   const vetBalance = BigInt(account.balance) / 1000000000000000000n;
   const vthoBalance = BigInt(account.energy) / 1000000000000000000n;
